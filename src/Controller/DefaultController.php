74f2731cffa0dabae47c6250ecb36455766a0ea5
STEPHAN GREGORY <gregory.stephan2018@campus-eni.fr>
2019-01-29 09:49:49 +0100

    initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('default/home.html.twig');
+        //return new Response('pifpaf');
+    }
+
+    /**
+     * @Route("/faq", name="faq")
+     * URL et nom de la route
+     */
+    public function  faq()
+    {
+        return $this->render('default/faq.html.twig');
+    }
+
+    /**
+     * @Route("/CGU", name="CGU")
+     *
+     */
+    public function CGU()
+    {
+        return $this->render('default/CGU.html.twig');
+    }
+
+    /**
+     * @Route("/fonctionnement", name="fonctionnement")
+     *
+     */
+    public function fonctionnement()
+    {
+        return $this->render('default/fonctionnement.html.twig');
+    }
+
+}
---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Question;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/questions", name="question_list")
+     */
+    public function list()
+    {
+        $questionRepository = $this
+            ->getDoctrine()
+            ->getRepository(Question::class);
+
+        $questions = $questionRepository->findBy(
+            ['status' => 'debating'], //where
+            ['supports' => 'DESC'], //order by
+            1000, //limit
+            0 //offset
+        );
+
+        //dd($questions);
+        //var dump et die()
+
+        return $this->render('question/list.html.twig', [
+                'questions' => $questions
+            ]);
+
+    }
+}
